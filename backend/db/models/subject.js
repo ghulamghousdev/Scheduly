5d85347af0e751a8aa69ac14de6f98b2b6800120
ghulamghousdev <itsghulamghous@gmail.com>
2020-07-17 01:14:56 +0500

    defined schema for subject model

---
+++ backend/db/models/subject.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+//Defining the subject schema for Subject Model
+const subjectSchema = mongoose.Schema({
+  //Defining the subject code on Subject Schema
+  subjectCode: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: [true, "Subject code is already taken"],
+    minLength: 5,
+  },
+
+  //Defining the subject name on Subject Schema
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  //Defining the Credit Hours on Subject Schema
+  creditHours: {
+    type: Number,
+    required: true,
+    validate(value) {
+      if (value < 0) {
+        throw new error("Credit hours can not be negative");
+      }
+      if (value > 3) {
+        throw new error("Credit hours cannot be more then 3");
+      }
+    },
+  },
+
+  //Defining the Contact Hours on Subject Schema
+  contactHours: {
+    type: Number,
+    require: true,
+    validate(value) {
+      if (value < 0) {
+        throw new error("Contact hours can not be negative");
+      }
+      if (value > 3) {
+        throw new error("Contact hours cannot be more then 3");
+      }
+    },
+  },
+});
+
+const Subject = mongoose.model("Subject", subjectSchema);
+
+module.exports = Subject;
