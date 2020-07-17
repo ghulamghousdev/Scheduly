b03ead377dcfb5917509ea0be8edab305e30e075
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-07-17 12:29:01 -0700

    Added Routes for Subjects

---
+++ backend/routes/subject.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const auth = require('../middlewares/auth');
+const Subject = require('../db/models/subject');
+
+const router = express.Router();
+
+//TO ADD A NEW SUBJECT
+router.post('/api/subject', auth, async (req, res)=>{
+    const subject = new Subject({
+        ...req.body,
+        author: req.user._id
+    });
+
+    try{
+        await subject.save();
+        res.status(200).send(subject);
+    } catch(err){
+        res.status(400).send(err);
+    }
+})
+
+//GET ALL SUBJECTS
+router.get('/api/subjects', auth, async (req, res)=>{
+    try{
+        await req.user.populate('subjects').execPopulate();
+        res.status(201).send(req.user.subjects);
+    } catch(err){
+        res.status(400).send(err);
+    }
+})
+
+//GET A SUBJECT BY SPECIFYING ITS _id
+router.get('/api/subject/:id', auth, async (req, res)=>{
+    const _id = req.params.id;
+    try {
+        console.log(req.params.id);
+        const subject = await Subject.findOne({_id, author: req.user._id});
+        if (!subject) {
+            return res.status(404).send();
+        }
+        res.send(subject);
+    } catch (e) {
+        res.status(502).send();
+    }
+})
+
+//EDIT A SUBJECT BY SPECIFYING ITS ID
+router.patch('/api/subject/:id', auth, async (req, res)=>{
+
+})
+
+//DELETE ALL SUBJECTS
+router.delete('/api/subjects', auth, async (req, res)=>{
+
+})
+
+//DELETE A SUBJECT BY SPECIFYING ITS ID
+router.delete('/api/subject/:id', auth, async(req,res)=>{
+    
+})
+
+//EXPORTING THE ROUTER TO BE USED IN OTHER FILES
+module.exports = router;
