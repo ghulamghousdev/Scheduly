b759f08e04eb0a65838ca7dc3801076450effb3e
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-07-26 10:21:25 -0700

    Initialized React Project with create-react-app

---
+++ frontend/src/components/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.scss';
+
+function LoginPage() {
+  return (
+    <div className="fp-container">
+      
+    </div>
+  );
+}
+
+export default App;
