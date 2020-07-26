aa1f027b1c0fb0e67f4705bc85d3e2e1462552ec
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-07-26 15:13:24 -0700

    Changed Server's port

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(classRouter);
 app.use(subjectRouter);
 
 // PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log("Server is up on port" + port);
