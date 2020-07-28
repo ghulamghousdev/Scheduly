83448e18541dd5edd7d8c454629f57475c5a1809
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-07-28 08:56:21 -0700

    Implemented Routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import TextBox from './textbox';
-import ButtonBox from './buttonbox';
-import SignUp from './signup';
-import '../styles/signupPage.scss';
-import Clock from '../clock-illustration.png'
+import {BrowserRouter, Route, Switch, Link, NavLink} from "react-router-dom";
+import UserCredentials from "./user";
 
-function LoginPage() {
+
+function App() {
   return (
-    <div className="fp-container">
-      <div className="row fw-row col-2-2-1">
-        <div className="col-1">
-          <TextBox 
-            preHeading="Welcome To"
-            heading="Scheduly"
-            text="This app can be used to generate an automatically scheduled time table. Now get rid of all the manual labour of hit and trial method for generating time table"
-          />
-          <img className="login-image" src={Clock}/>
-          <ButtonBox 
-            btOneClass="buttonbox__button buttonbox__button--active"
-            btTwoClass="buttonbox__button"
-          />
-        </div>
-        <div className="col-2">
-          <SignUp />
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/user">
+          <UserCredentials />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
-export default LoginPage;
+export default App;
