8b2099f720cebe44403aa5b3f1d68cbe3dd90130
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-07-28 13:53:32 -0700

    Added a route for addClass component

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -1,31 +1,43 @@
 import React from "react";
 import {BrowserRouter, Route, Switch, Link, NavLink, useParams, useRouteMatch} from "react-router-dom";
+import AddClass from "./addClass";
 import "../styles/dashboard.scss";
-import Logo from "../logo.svg"
+import Logo from "../logo.svg";
 
-const Dashboard = ()=>{
+function Dashboard(){
     let { path, url } = useRouteMatch();
-
-    const handleLogout= ()=>{
-
-    }
-
     return (
+        <BrowserRouter>
         <div className="dashboard row-2-1-5">
             <div className="sidebar">
                 <img className="logo" src={Logo} alt="Logo"/>
                 <ul className="menu">
                     <li className="menu__li">
-                        <NavLink className="menu__link menu__link--top" to={`{url}/classes/add`}>Classes</NavLink>
+                        <NavLink 
+                            className="menu__link menu__link--top" 
+                            to={`${url}/classes/add`} 
+                        >Classes</NavLink>
                     </li>
+
                     <li className="menu__li">
-                        <NavLink className="menu__link" to={`{url}/subjects/add`}>Subjects</NavLink>
-                        </li>
-                    <li className="menu__li">
-                        <NavLink className="menu__link" to={`{url}/teachers/add`}>Teachers</NavLink>
+                        <NavLink 
+                            className="menu__link" 
+                            to={`${url}/subjects/add`}
+                        >Subjects</NavLink>
                     </li>
+
                     <li className="menu__li">
-                        <NavLink className="menu__btn" to={`{url}/teachers/add`}>Generate</NavLink>
+                        <NavLink 
+                            className="menu__link" 
+                            to={`${url}/teachers/add`}
+                        >Teachers</NavLink>
+                    </li>
+
+                    <li className="menu__li">
+                        <NavLink 
+                            to="/"
+                            className="menu__btn"
+                        >Generate</NavLink>
                     </li>
                 </ul>
             </div>
@@ -33,14 +45,17 @@ const Dashboard = ()=>{
                 <div className="secondary-menubar">
                     <ul className="secondary-menu">
                         <li><Link className="secondary-menu__link" exact to="/">Home</Link></li>
-                        <li><button className="secondary-menu__btn" onClick={handleLogout}>Logout</button></li>
+                        <li><button className="secondary-menu__btn">Logout</button></li>
                     </ul>
                 </div>
                 <div className="component-container">
-
+                    <Route path={`${url}/classes/add`}>
+                        <AddClass />
+                    </Route>
                 </div>
             </div>
         </div>
+        </BrowserRouter>
     )
 }
 
