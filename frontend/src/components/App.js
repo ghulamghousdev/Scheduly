eeaec1999cf64f5d9850156721be8bf5cc27ee9b
ghulamghousdev <itsghulamghous@gmail.com>
2020-07-30 02:50:00 +0500

    formatting

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch, Link, NavLink} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import UserCredentials from "./User";
 import Dashboard from "./Dashboard";
-
 
 function App() {
   return (
@@ -12,7 +11,7 @@ function App() {
           <UserCredentials />
         </Route>
         <Route path="/dashboard">
-          <Dashboard/>
+          <Dashboard />
         </Route>
       </Switch>
     </BrowserRouter>
