54c699dff2c6905ab5bb76c2a9990ca3a079c0da
ghulamghousdev <itsghulamghous@gmail.com>
2020-07-30 02:06:17 +0500

    added add slots in side bar

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -1,72 +1,92 @@
 import React from "react";
-import {BrowserRouter, Route, Switch, Link, NavLink, useParams, useRouteMatch} from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+  NavLink,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 import AddClass from "./addClass";
 import AddSubject from "./addSubject";
 import AddTeacher from "./addTeacher";
+
 import "../styles/dashboard.scss";
 import Logo from "../logo.svg";
 
-function Dashboard(){
-    let { path, url } = useRouteMatch();
-    return (
-        <BrowserRouter>
-        <div className="dashboard row-2-1-5">
-            <div className="sidebar">
-                <img className="logo" src={Logo} alt="Logo"/>
-                <ul className="menu">
-                    <li className="menu__li">
-                        <NavLink 
-                            className="menu__link menu__link--top" 
-                            to={`${url}/classes/add`} 
-                        >Classes</NavLink>
-                    </li>
+function Dashboard() {
+  let { path, url } = useRouteMatch();
+  return (
+    <BrowserRouter>
+      <div className="dashboard row-2-1-5">
+        <div className="sidebar">
+          <img className="logo" src={Logo} alt="Logo" />
+          <ul className="menu">
+            <li className="menu__li">
+              <NavLink
+                className="menu__link menu__link--top"
+                to={`${url}/classes/add`}
+              >
+                Classes
+              </NavLink>
+            </li>
 
-                    <li className="menu__li">
-                        <NavLink 
-                            className="menu__link" 
-                            to={`${url}/subjects/add`}
-                        >Subjects</NavLink>
-                    </li>
+            <li className="menu__li">
+              <NavLink className="menu__link" to={`${url}/subjects/add`}>
+                Subjects
+              </NavLink>
+            </li>
 
-                    <li className="menu__li">
-                        <NavLink 
-                            className="menu__link" 
-                            to={`${url}/teachers/add`}
-                        >Teachers</NavLink>
-                    </li>
+            <li className="menu__li">
+              <NavLink className="menu__link" to={`${url}/teachers/add`}>
+                Teachers
+              </NavLink>
+            </li>
 
-                    <li className="menu__li">
-                        <NavLink 
-                            to="/"
-                            className="menu__btn"
-                        >Generate</NavLink>
-                    </li>
-                </ul>
-            </div>
-            <div className="right-container">
-                <div className="secondary-menubar">
-                    <ul className="secondary-menu">
-                        <li><Link className="secondary-menu__link" exact to="/">Home</Link></li>
-                        <li><button className="secondary-menu__btn">Logout</button></li>
-                    </ul>
-                </div>
-                <div className="component-container">
-                    <Switch>
-                        <Route path={`${url}/classes/add`}>
-                            <AddClass />
-                        </Route>
-                        <Route path={`${url}/subjects/add`}>
-                            <AddSubject />
-                        </Route>
-                        <Route path={`${url}/teachers/add`}>
-                            <AddTeacher />
-                        </Route>
-                    </Switch>
-                </div>
-            </div>
+            <li className="menu__li">
+              <NavLink className="menu__link" to={`${url}/slots/add`}>
+                Slots
+              </NavLink>
+            </li>
+
+            <li className="menu__li">
+              <NavLink to="/" className="menu__btn">
+                Generate
+              </NavLink>
+            </li>
+          </ul>
         </div>
-        </BrowserRouter>
-    )
+        <div className="right-container">
+          <div className="secondary-menubar">
+            <ul className="secondary-menu">
+              <li>
+                <Link className="secondary-menu__link" exact to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <button className="secondary-menu__btn">Logout</button>
+              </li>
+            </ul>
+          </div>
+          <div className="component-container">
+            <Switch>
+              <Route path={`${url}/classes/add`}>
+                <AddClass />
+              </Route>
+              <Route path={`${url}/subjects/add`}>
+                <AddSubject />
+              </Route>
+              <Route path={`${url}/teachers/add`}>
+                <AddTeacher />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default Dashboard;
