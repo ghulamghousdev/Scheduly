c7e6608be9d4a2c210ca90a61de1695a4fb2a884
ghulamghousdev <itsghulamghous@gmail.com>
2020-07-30 02:53:51 +0500

    New Route for Add slots

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -1,9 +1,18 @@
 import React from "react";
-import {BrowserRouter, Route, Switch, Link, NavLink, useParams, useRouteMatch} from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+  NavLink,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 import AddClass from "./AddClass";
 import AddSubject from "./AddSubject";
 import AddTeacher from "./AddTeacher";
-import ClassList from './ClassList';
+import ClassList from "./ClassList";
+import AddSlots from "./AddSlots";
 import "../styles/dashboard.scss";
 import Logo from "../logo.svg";
 
@@ -42,38 +51,45 @@ function Dashboard() {
               </NavLink>
             </li>
 
-                    <li className="menu__li">
-                        <NavLink 
-                            to="/"
-                            className="menu__btn"
-                        >Generate</NavLink>
-                    </li>
-                </ul>
-            </div>
-            <div className="right-container">
-                <div className="secondary-menubar">
-                    <ul className="secondary-menu">
-                        <li><Link className="secondary-menu__link" exact to="/">Home</Link></li>
-                        <li><button className="secondary-menu__btn">Logout</button></li>
-                    </ul>
-                </div>
-                <div className="component-container">
-                    <Switch>
-                        <Route path={`${url}/classes/add`}>
-                            <AddClass />
-                        </Route>
-                        <Route path={`${url}/subjects/add`}>
-                            <AddSubject />
-                        </Route>
-                        <Route path={`${url}/teachers/add`}>
-                            <AddTeacher />
-                        </Route>
-                        <Route path={`${url}/classes/view`}>
-                            <ClassList className="N-7" session="2018" section="A" />
-                        </Route>
-                    </Switch>
-                </div>
-            </div>
+            <li className="menu__li">
+              <NavLink to="/" className="menu__btn">
+                Generate
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="right-container">
+          <div className="secondary-menubar">
+            <ul className="secondary-menu">
+              <li>
+                <Link className="secondary-menu__link" exact to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <button className="secondary-menu__btn">Logout</button>
+              </li>
+            </ul>
+          </div>
+          <div className="component-container">
+            <Switch>
+              <Route path={`${url}/classes/add`}>
+                <AddClass />
+              </Route>
+              <Route path={`${url}/subjects/add`}>
+                <AddSubject />
+              </Route>
+              <Route path={`${url}/teachers/add`}>
+                <AddTeacher />
+              </Route>
+              <Route path={`${url}/slots/add`}>
+                <AddSlots />
+              </Route>
+              <Route path={`${url}/classes/view`}>
+                <ClassList className="N-7" session="2018" section="A" />
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
     </BrowserRouter>
