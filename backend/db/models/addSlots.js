dc7df42a62879a08d0d41f7b9894d41db73878e9
ghulamghousdev <itsghulamghous@gmail.com>
2020-07-31 21:30:41 +0500

    Rename file

---
+++ backend/db/models/addSlots.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+//Defining the ADD SLOTS schema for Add Slots Model
+const addSlotsSchema = mongoose.Schema({
+  //Defining the Teacher Name property on ADD SLOTS Schema
+  teacherName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  //Defining the subject property on ADD SLOTS Schema
+  subject: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  //Defining the section property on ADD SLOTS Schema
+  section: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  //Defining number of contact hours property on ADD SLOTS Schema
+  contactHours: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Contact hours can not be negative");
+      }
+      if (value > 3) {
+        throw new Error("Contact hours cannot be more then 3");
+      }
+    },
+  },
+  //Defining the relationship with the user
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
+
+const AddSlots = mongoose.model("AddSlots", addSlotsSchema);
+
+module.exports = AddSlots;
