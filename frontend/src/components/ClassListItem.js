739f6552c0263bd83d2631b925c809e9d71559de
ghulamghousdev <itsghulamghous@gmail.com>
2020-07-31 20:10:55 +0500

    Set Formatting

--- frontend/src/components/ClassListItem.js
+++ frontend/src/components/ClassListItem.js
@@ -1,20 +1,25 @@
 import React, { Children } from "react";
-import '../styles/classlistitem.scss';
+import "../styles/classlistitem.scss";
 
-function ClassListItem(props){
+function ClassListItem(props) {
+  const handleRemoveClass = () => {
+    //Calling The handler function passed through the props
+    props.remove(props.classCode);
+  };
 
-    const handleRemoveClass = ()=>{
-
-        //Calling The handler function passed through the props
-        props.remove(props.classCode);
-    }
-
-    return (
-        <div className="class">
-            <p className="class__details">{props.classCode} | {props.session} - {props.section}</p>
-            <button onClick={handleRemoveClass} className="class__btn class__btn--remove">Remove</button>
-        </div>
-    )
+  return (
+    <div className="class">
+      <p className="class__details">
+        {props.classCode} | {props.session} - {props.section}
+      </p>
+      <button
+        onClick={handleRemoveClass}
+        className="class__btn class__btn--remove"
+      >
+        Remove
+      </button>
+    </div>
+  );
 }
 
 export default ClassListItem;
