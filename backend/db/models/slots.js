4e164e246d57892198c2cfba8ba131439f4a4b8f
ghulamghousdev <itsghulamghous@gmail.com>
2020-08-04 02:49:31 +0500

    Renamed slots

---
+++ backend/db/models/slots.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+//Defining the ADD SLOTS schema for Add Slots Model
+const addSlotsSchema = mongoose.Schema({
+  //Defining the Teacher Name property on ADD SLOTS Schema
+  teacherName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  //Defining the subject property on ADD SLOTS Schema
+  subjectName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  //Defining the session property on AD SLOTS Schema
+  session: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  //Defining the section property on ADD SLOTS Schema
+  section: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  //Defining number of contact hours property on ADD SLOTS Schema
+  contactHours: {
+    type: Number,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Contact hours can not be negative");
+      }
+      if (value > 3) {
+        throw new Error("Contact hours cannot be more then 3");
+      }
+    },
+  },
+  //Defining the relationship with the user
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+
+  //DEFINING A FOREIGN RELATIONSHIP WITH SUBJECT
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Subject",
+  },
+  //DEFINING A FOREIGN RELATIONSHIP WITH TEACHER
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Teacher",
+  },
+});
+
+const AddSlots = mongoose.model("AddSlots", addSlotsSchema);
+
+module.exports = AddSlots;
