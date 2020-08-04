1a6ccc8f3c7c07f73c8bdf0d340d592b4f6dd287
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-08-04 15:04:47 -0700

    Handled HTTP request

--- frontend/src/components/AddClass.js
+++ frontend/src/components/AddClass.js
@@ -1,17 +1,47 @@
 import React from "react";
 import axios from "axios";
 import Class from './ClassListItem';
+import auth from "../utils/auth";
 import "../styles/addform.scss";
 
 class AddClass extends React.Component {
     constructor(){
         super();
+        this.handleAddClass = this.handleAddClass.bind(this);
+    }
+
+    async handleAddClass(e){
+        e.preventDefault();
+        const className = e.target.elements.className.value;
+        const session = e.target.elements.session.value;
+        const section = e.target.elements.section.value;
+
+        const classDetails = {
+            className,
+            session,
+            section
+        }
+
+        try{
+            const authToken = auth.getAuthToken();
+            const config = {
+                headers: {
+                    'Content-Type': 'Application/json',
+                    Authorization: `Bearer ${authToken}`
+                }
+            }
+            const body = JSON.stringify(classDetails);
+            const res = await axios.post('/api/class', body, config);
+            console.log(res.data);
+        } catch (err){
+            console.log(err);
+        }
     }
 
     render(){
         return(
-            <div class="addform">
-                <form>
+            <div className="addform">
+                <form onSubmit={this.handleAddClass}>
                     <h1 className="addform__heading">Add CLass</h1>
                     <input 
                         className="addform__input addform__input--full" 
