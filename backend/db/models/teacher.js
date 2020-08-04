c9773deb7aa9a91a6433608952dd729e501b6623
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-08-04 13:24:06 -0700

    Removed Unnecessary Relation

--- backend/db/models/teacher.js
+++ backend/db/models/teacher.js
@@ -44,6 +44,7 @@ const teacherSchema = mongoose.Schema({
     required: true,
     trim: true,
   },
+  
   //DEFINING A FOREIGN RELATIONSHIP WITH USER
   author: {
     type: mongoose.Schema.Types.ObjectId,
@@ -51,12 +52,6 @@ const teacherSchema = mongoose.Schema({
     ref: "User",
   },
 
-  //DEFINING A FOREIGN RELATIONSHIP WITH SUBJECT
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "Subjects",
-  },
 });
 
 const Teacher = mongoose.model("Teacher", teacherSchema);
