7a1f72d1c44760858a314e50d59ee6bc06a37e28
ghulamghousdev <itsghulamghous@gmail.com>
2020-08-08 23:14:02 +0500

    Removed Extra Comments

--- backend/routes/getData.js
+++ backend/routes/getData.js
@@ -12,34 +12,27 @@ router.get("/api/fetchdata", auth, async (req, res) => {
   let subjectArray = [];
   let teacherArray = [];
   let slotsArray = [];
-  let labs = [];
   let givenSlots = [7, 7, 7, 7, 4];
+  console.log("ddd");
   try {
     let sectionData = await Section.find({ author: req.user._id });
     let subjectData = await Subject.find({ author: req.user._id });
     let teacherData = await Teacher.find({ author: req.user._id });
     let slotsData = await Slots.find({ author: req.user._id });
-    // console.log(sectionData);
-
+    console.log("WER");
     for (let i = 0; i < sectionData.length; i++) {
       sectionArray[i] = `${sectionData[i].section}-${sectionData[i].session}`;
     }
 
-    //console.log(sectionArray);
-
-    //console.log(subjectData);
     for (let i = 0; i < subjectData.length; i++) {
       subjectArray[i] = `${subjectData[i].subjectName}`;
     }
-
-    //console.log(subjectArray);
 
     for (let i = 0; i < teacherData.length; i++) {
       teacherArray[
         i
       ] = `${teacherData[i].firstName} ${teacherData[i].lastName}`;
     }
-    //console.log(teacherArray);
 
     for (let j = 0; j < slotsData.length; j++) {
       let slotsObj = {};
@@ -51,29 +44,12 @@ router.get("/api/fetchdata", auth, async (req, res) => {
       slotsArray.push(slotsObj);
     }
 
-    console.log(sectionArray);
-    // console.log(teacherArray);
-    // console.log(givenSlots);
-
-    //     {
-    //       teacher: "W",
-    //       sections: ["B2"],
-    //       subject: "OS",
-    //       numLectures: "3",
-    //       numLabs: null,
-    //     },
-    //   ],
-    //   [7, 7, 7, 7, 7],
-
-    // ["A"]
     const resultedTimeTable = await TimeTable(
       slotsArray,
       givenSlots,
       teacherArray,
       sectionArray
     );
-
-    // console.log(TimeTable);
 
     res.status(200).send(resultedTimeTable);
   } catch (err) {
