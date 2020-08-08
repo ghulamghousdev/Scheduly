2a6e5420a7ec164dd247e86e0ec0114e9219a7d8
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-08-08 02:22:03 -0700

    Added HTTP request for Adding a slot

--- frontend/src/components/AddSlots.js
+++ frontend/src/components/AddSlots.js
@@ -1,16 +1,50 @@
 import React from "react";
 import axios from "axios";
+import auth from "../utils/auth"
 import "../styles/addform.scss";
 
 class AddSlots extends React.Component {
   constructor() {
     super();
+    this.handleAddSlots = this.handleAddSlots.bind(this);
   }
+
+  async handleAddSlots(e){
+    e.preventDefault();
+    const teacherName = e.target.elements.teacherName.value;
+    const session = e.target.elements.session.value;
+    const section = e.target.elements.section.value;
+    const subjectName = e.target.elements.subjectName.value;
+    const contactHours = e.target.elements.contactHours.value;
+
+    const slotDetails = {
+        teacherName,
+        subjectName,
+        session,
+        section,
+        contactHours
+    }
+
+    try{
+        const authToken = auth.getAuthToken();
+        const config = {
+            headers: {
+                'Content-Type': 'Application/json',
+                Authorization: `Bearer ${authToken}`
+            }
+        }
+        const body = JSON.stringify(slotDetails);
+        const res = await axios.post('/api/slot ', body, config);
+        console.log(res.data);
+    } catch (err){
+        console.log(err);
+    }
+}
 
   render() {
     return (
       <div class="addform">
-        <form>
+        <form onSubmit={this.handleAddSlots}>
           <h1 className="addform__heading">Add Slots</h1>
           <input
             className="addform__input addform__input--full"
