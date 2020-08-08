ddc5e5d92e617050547028d7f7ed8f9dace1d04c
ghulamghousdev <itsghulamghous@gmail.com>
2020-08-08 22:46:54 +0500

    Fixed data manipulation

--- backend/routes/getData.js
+++ backend/routes/getData.js
@@ -12,54 +12,70 @@ router.get("/api/fetchdata", auth, async (req, res) => {
   let subjectArray = [];
   let teacherArray = [];
   let slotsArray = [];
-  let slotsObj = {};
-  let givenSlots = [9, 9, 9, 9, 9];
+  let labs = [];
+  let givenSlots = [7, 7, 7, 7, 4];
   try {
     let sectionData = await Section.find({ author: req.user._id });
     let subjectData = await Subject.find({ author: req.user._id });
     let teacherData = await Teacher.find({ author: req.user._id });
     let slotsData = await Slots.find({ author: req.user._id });
-    //console.log(slotsData);
+    // console.log(sectionData);
 
     for (let i = 0; i < sectionData.length; i++) {
-      sectionArray[i] = `${sectionData[i].session}-${sectionData[i].section}`;
+      sectionArray[i] = `${sectionData[i].section}-${sectionData[i].session}`;
     }
 
+    //console.log(sectionArray);
+
+    //console.log(subjectData);
     for (let i = 0; i < subjectData.length; i++) {
-      subjectArray[i] = `${subjectData[i.subjectName]}`;
+      subjectArray[i] = `${subjectData[i].subjectName}`;
     }
+
     //console.log(subjectArray);
 
     for (let i = 0; i < teacherData.length; i++) {
-      teacherArray[i] = `${teacherData[i.firstName]} ${
-        teacherData[i.lastName]
-      }`;
+      teacherArray[
+        i
+      ] = `${teacherData[i].firstName} ${teacherData[i].lastName}`;
     }
     //console.log(teacherArray);
 
     for (let j = 0; j < slotsData.length; j++) {
-      slotsObj.teacher = `${slotsData[j.teacherName]}`;
-      slotsObj.sections = `[${slotsData[j.session]}-${slotsData[j.section]}]`;
-      slotsObj.subject = `${slotsData[j.subjectName]}`;
-      slotsObj.numLectures = `${slotsData[j.contactHours]}`;
-      for (let i = 0; i < subjectData.length; i++) {
-        if (`${slotsData[j.subjectName]}` === `${subjectData[i.subjectName]}`) {
-          slotsObj.labs = `${subjectData[i.labs]}`;
-          break;
-        }
-      }
-      slotsArray[j] = slotsObj;
+      let slotsObj = {};
+      slotsObj.teacher = `${slotsData[j].teacherName}`;
+      slotsObj.sections = [`${slotsData[j].session}-${slotsData[j].section}`];
+      slotsObj.subject = `${slotsData[j].subjectName}`;
+      slotsObj.numLectures = `${slotsData[j].contactHours}`;
+      slotsObj.numLabs = null;
+      slotsArray.push(slotsObj);
     }
-    //console.log(slotsData);
 
-    const resultedTimeTable = TimeTable(
+    console.log(sectionArray);
+    // console.log(teacherArray);
+    // console.log(givenSlots);
+
+    //     {
+    //       teacher: "W",
+    //       sections: ["B2"],
+    //       subject: "OS",
+    //       numLectures: "3",
+    //       numLabs: null,
+    //     },
+    //   ],
+    //   [7, 7, 7, 7, 7],
+
+    // ["A"]
+    const resultedTimeTable = await TimeTable(
       slotsArray,
       givenSlots,
       teacherArray,
       sectionArray
     );
 
-    res.status(200).send("resultedTimeTable");
+    // console.log(TimeTable);
+
+    res.status(200).send(resultedTimeTable);
   } catch (err) {
     res.status(400).send(err);
   }
