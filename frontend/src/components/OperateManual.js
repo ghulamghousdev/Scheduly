7dc39e3bd46ca575bf617089f056a48daebff5a3
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-08-13 16:44:02 -0700

    Changed description of the Steps

--- frontend/src/components/OperateManual.js
+++ frontend/src/components/OperateManual.js
@@ -18,17 +18,17 @@ const OperateManual = (props)=>{
             <Step 
                 number="3" 
                 heading="Add Subjects"
-                instruction="Add all the classroom available in your department and assign one class to each section of every session."    
+                instruction="Add all the Subject that you are offering. There is no need to add a seperate subject for Lab"    
             />
             <Step 
                 number="4"
                 heading="Add Slots"
-                instruction="Add all the classroom available in your department and assign one class to each section of every session."    
+                instruction="Here you will be asked to provide a Slot for each lecture. Means to assign a teacher to a subject which will teach a specific section of a session."    
             />
             <Step 
                 number="5" 
                 heading="Generate"
-                instruction="Add all the classroom available in your department and assign one class to each section of every session."    
+                instruction="And finally Click generate and bang. You have created your timetable successfully"    
                 extraClass="step--last"
             />
         </div>
