0f7395446d69ec2a0b7b5733b1f0e57d0c6ccfdb
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-08-13 16:45:35 -0700

    Added a new component: Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/footer.scss";
+
+const Footer = ()=>{
+    return(
+        <section className="footer">
+            <p className="credits">A project by <a className="credits__link"href="#">@ghulamghousdev</a> and <a className="credits__link"href="#">@ahmadzaheer-dev</a></p>
+        </section>
+    )
+}
+
+export default Footer;
