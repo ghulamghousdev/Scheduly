5644e0daca42a2eee236b0a1357b5b1da099975e
Ghulam Ghous <Itsghulamghous@gmail.com>
2020-08-14 13:45:52 +0500

    added github links

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ import "../styles/footer.scss";
 const Footer = ()=>{
     return(
         <section className="footer">
-            <p className="credits">A project by <a className="credits__link"href="#">@ghulamghousdev</a> and <a className="credits__link"href="#">@ahmadzaheer-dev</a></p>
+            <p className="credits">A project built with ❤ by <a className="credits__link"href="https://github.com/ghulamghousdev">@ghulamghousdev</a> and <a className="credits__link"href="https://github.com/ahmadzaheer-dev">@ahmadzaheer-dev</a></p>
         </section>
     )
 }
