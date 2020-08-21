fdeb2d8fea8bd7f65e4cc20a11cc96bc9428bcdb
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-08-21 07:38:03 -0700

    Improved HomePage

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -7,50 +7,65 @@ import Logo from "../logo.svg";
 import Footer from "./Footer";
 import OperateManual from "./OperateManual";
 
-const HomePage = (props)=>{
-    return(
-        <div>
-            <section className="header">
-                <div className="home-nav">
-                    <div className="home-row ">
-                        <h2 className="home-logo">Scheduly</h2>
-                        <ul className="home-menu">
-                            <li className="home-menu__li"><a className="home-menu__link home-menu__link--simple" href="/user/login">Sign In</a></li>
-                            <li className="home-menu__li"><a className="home-menu__link home-menu__link--btn" href="/user/signup">Join</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="home-row fw-row col-2-1-2">
-                    <div className="col">
-                        <div className="textbox--home">
-                            <h2 className="textbox__preHeading--blue">Welcome to</h2>
-                            <h1 className="textbox__heading--dark">Scheduly</h1>
-                            <p>This app can be used to generate an automatically scheduled time table. Now get rid of all the manual labour of hit and trial method for generating time table</p>
-                            <a href="#" className="about-btn">About</a>
-                        </div>
-                    </div>
-                    <div className="col">
-                    </div>
-                </div>
-            </section>
-            <section className="manual">
-                <div className="home-row fw-row col-2-1-2">
-                    <div className="col">
-                        <div className="textbox--guide">
-                            <h2 className="textbox__preHeading--blue">User Guide</h2>
-                            <h1 className="textbox__heading--dark">Follow the described steps</h1>
-                        </div>
-                
-                    </div>
-                    <div className="col">
-                        <OperateManual/>
-                    </div>
+class HomePage extends React.Component{
+    constructor(){
+        super();
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
 
-                </div>                
-            </section>
-            <Footer />
-        </div>
-    )
+    componentDidMount(){
+        const token = auth.getAuthToken();
+        if(token){
+            window.location.href = "/dashboard";
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <section className="header">
+                    <div className="home-nav">
+                        <div className="home-row ">
+                            <h2 className="home-logo">Scheduly</h2>
+                            <ul className="home-menu">
+                                <li className="home-menu__li"><a className="home-menu__link home-menu__link--simple" href="/user/login">Sign In</a></li>
+                                <li className="home-menu__li"><a className="home-menu__link home-menu__link--btn" href="/user/signup">Join</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="home-row fw-row col-2-1-2">
+                        <div className="col">
+                            <div className="textbox--home">
+                                <h2 className="textbox__preHeading--blue">Welcome to</h2>
+                                <h1 className="textbox__heading--dark">Scheduly</h1>
+                                <p>This app can be used to generate an automatically scheduled time table. Now get rid of all the manual labour of hit and trial method for generating time table</p>
+                                <a href="#" className="about-btn">About</a>
+                            </div>
+                        </div>
+                        <div className="col">
+                        </div>
+                    </div>
+                </section>
+                <section className="manual">
+                    <div className="home-row fw-row col-2-1-2">
+                        <div className="col">
+                            <div className="textbox--guide">
+                                <h2 className="textbox__preHeading--blue">User Guide</h2>
+                                <h1 className="textbox__heading--dark">Follow the described steps</h1>
+                            </div>
+                    
+                        </div>
+                        <div className="col">
+                            <OperateManual/>
+                        </div>
+    
+                    </div>                
+                </section>
+                <Footer />
+            </div>
+        )
+    }
+    
 }
 
 export default HomePage;
