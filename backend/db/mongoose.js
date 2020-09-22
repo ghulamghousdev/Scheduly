80442677005f6df41a27f75d97f4690aa93acf00
ghulamghousdev <itsghulamghous@gmail.com>
2020-09-22 21:10:15 +0500

    changed db from local to cloud

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 //CONNECTING TO DATABASE
 mongoose.connect(
-  "mongodb+srv://ghulamghousdev:ghulamghous826@scheduly.1eln0.mongodb.net/test?retryWrites=true&w=majority",
+  "mongodb+srv://ghulamghousdev:ghulamghous826@scheduly.1eln0.mongodb.net/timetable?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
