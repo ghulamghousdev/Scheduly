b0fb4c1ed0cafbd10c42ebb28bce782106ab57d6
ghulamghousdev <itsghulamghous@gmail.com>
2021-01-06 16:13:05 +0500

    Removed warnings

--- frontend/src/components/AddClass.js
+++ frontend/src/components/AddClass.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import Class from "./ClassListItem";
 import auth from "../utils/auth";
 import "../styles/addform.scss";
 
--- frontend/src/components/AddSubject.js
+++ frontend/src/components/AddSubject.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import Subject from "./SubjectListItem";
 import auth from "../utils/auth";
 import "../styles/addform.scss";
 
