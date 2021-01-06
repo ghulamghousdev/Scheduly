b187894d458c1e04ab39977c534d98c446d2b858
ghulamghousdev <itsghulamghous@gmail.com>
2021-01-06 16:20:05 +0500

    Added error handling

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import auth from '../utils/auth';
+import {Link} from 'react-router-dom';
 import '../styles/form.scss';
-
+import {
+    Message
+  } from "semantic-ui-react";
 
 class Login extends React.Component {
+
+    state = {
+        errors: []
+    };
     handleFormSubmit = async e =>{
         e.preventDefault();
         const email = e.target.elements.email.value;
@@ -14,25 +21,56 @@ class Login extends React.Component {
             email,
             password
         }
-
-        try{
-            const config = {
-                headers: {
-                    'Content-Type': 'Application/json'
+      
+        if(this.isFormValid(credentials)){
+            this.setState({
+                errors: []
+              });
+            try{
+                const config = {
+                    headers: {
+                        'Content-Type': 'Application/json'
+                    }
                 }
+                const body = JSON.stringify(credentials);
+                const res = await axios.post('/api/user/login', body, config);
+                auth.setAuthToken(res.data.token);
+                if(res.data.token){
+                    window.location.href="/dashboard";
+                }
+                
+            } catch(e){
+                console.log(e); 
+                let error={message: "Something went wrong. Try again!"};
+                let errors = [];
+                this.setState({
+                    errors: errors.concat(error)
+                })
             }
-            const body = JSON.stringify(credentials);
-            const res = await axios.post('/api/user/login', body, config);
-            auth.setAuthToken(res.data.token);
-            if(res.data.token){
-                window.location.href="/dashboard";
-            }
-            
-        } catch(e){
-            console.log(e);
         }
-        
     }
+        
+    
+
+    isFormValid = (credentials) =>{
+        let errors = [];
+        let error;
+        if(this.isFormEmpty(credentials)){
+            error = {message : 'Fill in all the fields'};
+            this.setState({errors: errors.concat(error)})
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    isFormEmpty = ({email, password})=>{
+        return ( !password.length || !email.length);
+    }
+
+    displayErrors = (errors) =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
 
     render(){
         return (
@@ -56,6 +94,14 @@ class Login extends React.Component {
               name="submit"
               value="Login"
             />
+            {this.state.errors.length > 0 && (
+              <Message className="errorclass">
+                {this.displayErrors(this.state.errors)}
+              </Message>
+            )}            
+            <Message style={{marginTop: 30}}>
+                Don't have an account? <Link to="/user/signup" >Sign Up</Link>
+            </Message>
           </form>
         )
     }
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -2,12 +2,17 @@ import React from 'react';
 import axios from 'axios';
 import auth from '../utils/auth'
 import '../styles/form.scss';
+import {Link} from "react-router-dom"
+import {
+  Message
+} from "semantic-ui-react";
 
 
 class SignUp extends React.Component {
-    constructor(){
-        super();
-    }
+    state = {
+      errors: []
+    };
+
     handleFormSubmit = async e =>{
         e.preventDefault();
         const email = e.target.elements.email.value;
@@ -15,32 +20,94 @@ class SignUp extends React.Component {
         const lastName = e.target.elements.lastname.value;
         const password = e.target.elements.password.value;
         const confirmedPassword = e.target.elements.confirm.value;
+        const userData ={
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmedPassword
+        }
 
-        if(password !== confirmedPassword){
-            console.log("Passwords do not match");
-        } else{
-            const newUser = {
-                firstName,
-                lastName,
-                email,
-                password
+        if(this.isFormValid(userData)){
+          this.setState({
+            errors: []
+          });
+          const newUser = {
+              firstName,
+              lastName,
+              email,
+              password
             }
-
-            try{
-                const config = {
-                    headers: {
-                        'Content-Type': 'Application/json'
+          try{
+              const config = {
+                  headbers: {
+                      'Content-Type': 'Application/json'
                     }
                 }
-                const body = JSON.stringify(newUser);
-                const res = await axios.post('/api/user', body, config);
-                auth.setAuthToken(res.data.token);
-                
-            } catch(e){
-                console.log(e);
+              const body = JSON.stringify(newUser);
+              const res = await axios.post('/api/user/signup', body, config);
+              auth.setAuthToken(res.data.token);
+              if(true){
+                window.location.href="/user/login";
+            }
+          } catch(e){
+            console.log(e);
+            let error={message: "Something went wrong. Try again!"};
+            let errors = [];
+            this.setState({
+              errors: errors.concat(error)
+            })
             }
         }
     }
+
+    isFormValid = (userData) =>{
+        let errors = [];
+        let error;
+        if(this.isFormEmpty(userData)){
+            error = {message : 'Fill in all the fields'};
+            this.setState({errors: errors.concat(error)})
+            return false;
+        }
+        else if (!this.isPasswordValid(userData)){
+            error = { message: "Password should at least be 8 characters long" };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        }
+        else if (!this.isPasswordMatching(userData)){
+            error = { message: "Password do not match" };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    isFormEmpty = ({firstName, lastName, email, password, confirmedPassword})=>{
+      return (!firstName.length || !lastName.length || !email.length || !password.length || !confirmedPassword.length);
+    }
+
+    isPasswordValid = ({password, confirmedPassword}) => {
+      if (password.length < 8 || confirmedPassword.length < 8) {
+        return false;
+      } 
+      else {
+        return true;
+      }
+    }
+
+    isPasswordMatching = ({password, confirmedPassword}) => {
+      if (password !== confirmedPassword ) {
+        return false;
+      } 
+      else {
+        return true;
+      }
+    }
+
+    displayErrors = (errors) =>
+        errors.map((error, i) => <p key={i}>{error.message}</p>);
 
     render(){
         return (
@@ -49,7 +116,7 @@ class SignUp extends React.Component {
             <input 
               className="form__input" 
               placeholder="First Name" 
-              type="text" 
+              type="text"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
               name="firstname"
             />
             <input 
@@ -82,7 +149,16 @@ class SignUp extends React.Component {
               name="submit"
               value="Sign Up"
             />
+            {this.state.errors.length > 0 && (
+              <Message className="errorclass">
+                {this.displayErrors(this.state.errors)}
+              </Message>
+            )}
+            <Message style={{marginTop: 30}}>
+                Already a user? <Link to="/user/login">Login</Link>
+            </Message>
           </form>
+
         )
     }
 }
