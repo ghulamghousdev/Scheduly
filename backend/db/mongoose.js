dffeb15af91bd74cb1a612c2dabebbc89338f7a0
ghulamghousdev <itsghulamghous@gmail.com>
2021-01-06 15:50:38 +0500

    Added Mongo URI

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
 //CONNECTING TO DATABASE
-mongoose.connect(
-  "mongodb+srv://ghulamghousdev:ghulamghous826@scheduly.1eln0.mongodb.net/timetable?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose
+  .connect(
+    "mongodb://ghulamghousdev:ghulamghous826@scheduly-shard-00-00.1eln0.mongodb.net:27017,scheduly-shard-00-01.1eln0.mongodb.net:27017,scheduly-shard-00-02.1eln0.mongodb.net:27017/timetable?ssl=true&replicaSet=atlas-avxmlz-shard-0&authSource=admin&retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+  )
+  .then(() => {
+    console.log("Connected to database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
