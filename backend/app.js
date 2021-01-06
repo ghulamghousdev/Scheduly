c4108d47f78b2d2417871cd15e6aff9fe4cad0e9
ghulamghousdev <itsghulamghous@gmail.com>
2021-01-06 15:50:02 +0500

    Added production script for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
+"use strict"
 const express = require("express");
 const app = express();
 require("./db/mongoose");
+const cors = require('cors');
+const path = require('path');
 
 const userRouter = require("./routes/user");
 const subjectRouter = require("./routes/subject");
@@ -9,7 +12,11 @@ const teacherRouter = require("./routes/teacher");
 const slotsRouter = require("./routes/slots");
 const algoExecutionRouter = require("./routes/getData");
 
+app.use(express.static(path.join(__dirname, '../' + "/frontend/build")));
+
+app.use(cors())
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(userRouter);
 app.use(teacherRouter);
 app.use(classRouter);
@@ -17,8 +24,16 @@ app.use(subjectRouter);
 app.use(slotsRouter);
 app.use(algoExecutionRouter);
 
+
 // PORT
 const port = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static(path.join(__dirname, '../' + "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../' + "/frontend/build/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log("Server is up on port" + port);
